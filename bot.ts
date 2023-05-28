5c8b2113ed3a86bd516a8b93c434475ae95d3f7a
elintendo <elintendo@protonmail.com>
2023-05-28 16:33:51 +0300

    initial

--- bot.ts
+++ bot.ts
@@ -1,6 +1,5 @@
 import { Bot } from './deps.deno.ts'
-import { token } from './.token.ts'
-export const bot = new Bot(Deno.env.get(token) || '')
+export const bot = new Bot(Deno.env.get(Deno.env.get('token')) || '')
 
 bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
 
