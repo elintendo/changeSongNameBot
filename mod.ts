875cad140916506c53070ca7c41cdb9dd49c474c
elintendo <elintendo@protonmail.com>
2023-05-28 15:50:43 +0300

    initial

--- mod.ts
+++ mod.ts
@@ -1,20 +1,20 @@
-import { serve } from "https://deno.land/std@0.177.1/http/server.ts";
-import { webhookCallback } from "https://deno.land/x/grammy@v1.16.1/mod.ts";
+import { serve } from 'https://deno.land/std@0.177.1/http/server.ts'
+import { webhookCallback } from 'https://deno.land/x/grammy@v1.16.1/mod.ts'
 // You might modify this to the correct way to import your `Bot` object.
-import bot from "./bot.ts";
+import { bot } from './bot.ts'
 
-const handleUpdate = webhookCallback(bot, "std/http");
+const handleUpdate = webhookCallback(bot, 'std/http')
 
 serve(async (req) => {
-  if (req.method === "POST") {
-    const url = new URL(req.url);
+  if (req.method === 'POST') {
+    const url = new URL(req.url)
     if (url.pathname.slice(1) === bot.token) {
       try {
-        return await handleUpdate(req);
+        return await handleUpdate(req)
       } catch (err) {
-        console.error(err);
+        console.error(err)
       }
     }
   }
-  return new Response();
-});
+  return new Response()
+})
